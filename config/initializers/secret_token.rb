7ac1188cf40eb1198a10635b9252fd95d1a8b905
howar <hrasguido@hotmail.com>
2011-08-14 01:01:12 -0400

    create repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bla::Application.config.secret_token = '9d707ed4b5626e2082e98601ca419a58a1c00a4ce6c48b96644b098a6eb85284edc233cb2d81714e2e8753b1a37941e9a46ec419eb6eb55ccc123bf0f28f4bac'
